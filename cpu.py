c515574f97c91d565fbbea4c1ef1b160f8c7b009
bessieTheBoy <jaczkieboy33999@gmail.com>
2023-04-30 10:09:20 -0400

    added fixes

--- cpu.py
+++ cpu.py
@@ -4,6 +4,7 @@ from memory import MEMORY
 
 class CPU:
 	registers = {"a": 0, "x": 0, "y": 0}
+	last_instruction = 0
 	arg_reg = 0
 	mem = MEMORY()
 	history = []
@@ -32,14 +33,17 @@ class CPU:
 				print("END OF FILE shutting down (tip: remember to use 'brk 0' at the end of files)")
 				exit()
 			if byte == 0xf2:
-				self.run_inst_reg = LDA()
+				self.run_inst_reg = LDA(self)
 				self.pg += 1
+				 
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xf3:
-				self.run_inst_reg = JMP()
+				self.run_inst_reg = JMP(self)
+				 
 				self.pg += 1
 			elif byte == 0x99:
-				self.run_inst_reg = STA()
+				self.run_inst_reg = STA(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xf9:
@@ -49,65 +53,75 @@ class CPU:
 				starting_int = True
 				self.pg += 1
 			elif byte == 0xf5:
-				self.run_inst_reg = RD()
+				self.run_inst_reg = RD(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0x83:
-				self.run_inst_reg = STX()
+				self.run_inst_reg = STX(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0x85:
-				self.run_inst_reg = STY()
+				self.run_inst_reg = STY(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0x95:
-				self.run_inst_reg = LDX()
+				self.run_inst_reg = LDX(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0x93:
-				self.run_inst_reg = LDY()
+				self.run_inst_reg = LDY(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0x92:
-				self.run_inst_reg = ADD()
+				self.run_inst_reg = ADD(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xd2:
-				self.run_inst_reg = MUL()
+				self.run_inst_reg = MUL(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0x97:
-				self.run_inst_reg = SUB()
+				self.run_inst_reg = SUB(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xd5:
-				self.run_inst_reg = DIV()
+				self.run_inst_reg = DIV(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xd9:
-				self.run_inst_reg = LDXM()
+				self.run_inst_reg = LDXM(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xd8:
-				self.run_inst_reg = LDYM()
+				self.run_inst_reg = LDYM(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xd7:
-				self.run_inst_reg = IN()
+				self.run_inst_reg = IN(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			elif byte == 0xfc:
-				self.run_inst_reg = BRK()
+				self.run_inst_reg = BRK(self)
+				 
 				self.pg += 1
 				history.append({"inst": self.run_inst_reg, "arg_reg": self.arg_reg})
 			else:
-				if starting_mem_address:
-					self.arg_reg = byte
-					self.run_inst_reg.process(self)
-					self.pg += 1
-				elif starting_int:
-					self.arg_reg = byte
-					self.run_inst_reg.process(self)
-					self.pg += 1
+				self.arg_reg = byte
+				self.run_inst_reg.process(self)
+				self.last_instruction = self.run_inst_reg
+				self.pg += 1
+					
 			self.history = history
 
--- instructions.py
+++ instructions.py
@@ -1,84 +1,121 @@
 class Instruction:
+	def __init__(self, cpu):
+		self.cpu = cpu
+		self.arg_reg = cpu.arg_reg
 	def process(self, cpu):
 		pass
 
 
 class LDA(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.registers["a"] = cpu.arg_reg
 
 
 class LDX(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.registers["x"] = cpu.arg_reg
 
 
 class LDY(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.registers["y"] = cpu.arg_reg
 
 
 class STA(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, cpu.registers["a"])
 
 
 class STX(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, cpu.registers["x"])
 
 
 class STY(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, cpu.registers["y"])
 
 
 class RD(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		print(cpu.mem.read(cpu.arg_reg))
 
 
 class ADD(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, cpu.registers["x"] + cpu.registers["y"])
 
 
 class MUL(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, cpu.registers["x"] * cpu.registers["y"])
 
 
 class SUB(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, cpu.registers["x"] - cpu.registers["y"])
 
 
 class DIV(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, cpu.registers["x"] / cpu.registers["y"])
 
 
 class LDXM(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.registers["x"] = cpu.mem.read(cpu.arg_reg)
 
 
 class LDYM(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.registers["y"] = cpu.mem.read(cpu.arg_reg)
 
 
 class IN(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.mem.write(cpu.arg_reg, int(input("INPUT: ")))
 
 
 class JMP(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		cpu.run_file("test.rom", start_byte=cpu.arg_reg)
 
 
+
+
 class BRK(Instruction):
+	def __init__(self, cpu):
+		super().__init__(cpu)
 	def process(self, cpu):
 		print("BRK called shutting down...")
 		print(f"EXIT CODE: {cpu.arg_reg}")
